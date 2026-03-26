627b365a9ed9711124f15d861a9bae2f673f234a
mesutdalkilic <mesutdlklc@gmail.com>
2026-03-26 08:22:33 +0300

    feat: implement rate limiting with custom 429 response

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using UrlShortener.Data;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,9 +18,51 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
+
+// Redis Cache Port: 6379, Host: localhost
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = "localhost:6379";
+});
+
 // Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// 1 kullanýcý  saniyede X istek, API’yi olaðan dýþý aþýrý istekten korumak için rate limiting uygularýz
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = 429; // Ýstek reddedildiðinde dönecek HTTP durum kodu (Too Many Requests)
+
+    options.OnRejected = async (context, token) =>
+    {
+        // Response tipi JSON
+        context.HttpContext.Response.ContentType = "application/json";
+
+        // JSON body dön
+        var response = new
+        {
+            error = "Too many requests"
+        };
+
+        var json = System.Text.Json.JsonSerializer.Serialize(response);
+
+        await context.HttpContext.Response.WriteAsync(json, token);
+    };
+
+
+    options.AddFixedWindowLimiter("fixed", opt =>
+    {
+        opt.PermitLimit = 3; // 3 istek max istek sayýsý
+        opt.Window = TimeSpan.FromSeconds(10); // 10 saniyede
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        opt.QueueLimit = 0; // bekleyen istek sayýsý
+    });
+
+    
+});
+
 
 var app = builder.Build();
 
@@ -28,6 +74,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRateLimiter();
 
 app.UseAuthorization();
 
