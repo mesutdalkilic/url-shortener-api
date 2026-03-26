40ce56bd3c843a31fad3fe290d80413f5961beca
mesutdalkilic <mesutdlklc@gmail.com>
2026-03-26 08:20:31 +0300

    feat: implement Base62 encoding

---
+++ UrlShortener/Utils/Base62Service.cs
@@ -0,0 +1,25 @@
+﻿namespace UrlShortener.Utils;
+
+public static class Base62Service
+{
+    // Base62 karakter seti
+    private const string Characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static string Encode(int value)
+    {
+        // Güvenlik: 0 kontrolü
+        if (value == 0)
+            return "0";
+
+        var result = string.Empty;
+
+        // Base62 encoding algoritması
+        while (value > 0)
+        {
+            result = Characters[value % 62] + result;
+            value /= 62;
+        }
+
+        return result;
+    }
+}
